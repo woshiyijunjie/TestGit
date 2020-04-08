39912006c9f3956c5c99353818a512743d3173d9
yjj <15828452015@163.com>
2020-04-08 14:14:12 +0800

    new hot_fix

--- src/main/java/com/junjie/git/Apple.java
+++ src/main/java/com/junjie/git/Apple.java
@@ -4,6 +4,7 @@ public class Apple {
 	public static void main(String[] args) {
 		System.out.println("gitclone");
 		System.out.println("githubtest");
+		System.out.println("hot_fix");
 		System.out.println("yijunjie");
 	}
 }
